bfcfa0743f61aa6acc938face90f51a80d9ad543
lepton1 <zilvinaszelvys@gmail.com>
2018-08-05 18:25:51 +0200

    i18next

--- diana/src/App.js
+++ diana/src/App.js
@@ -1,21 +1,55 @@
 import React, { Component } from 'react';
+import { I18n, Trans } from 'react-i18next';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <I18n ns="translations">
+        {
+          (t, { i18n }) => (
+            <div className="App">
+              <div className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h2>{t('title')}</h2>
+                <button onClick={() => i18n.changeLanguage('es')}>de</button>
+                <button onClick={() => i18n.changeLanguage('en')}>en</button>
+              </div>
+              <div className="App-intro">
+                <Trans i18nKey="description.part1">
+                  To get started, edit <code>src/App.js</code> and save to reload.
+                </Trans>
+              </div>
+              <div>{t('description.part2')}</div>
+            </div>
+          )
+        }
+      </I18n>
     );
   }
 }
 
 export default App;
+
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
